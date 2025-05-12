b497aeab44dbfc797e23d35335cbe6ec3b099441
Vansh Jain <131325445+JainVansh03@users.noreply.github.com>
2025-05-12 23:30:58 +0530

    Create Nested_Lists.py

---
+++ Nested_Lists.py
@@ -0,0 +1,36 @@
+        
+if __name__ == '__main__':
+    list1 = list()
+    for _ in range(int(input())):
+        name = input()
+        score = input()
+        list1.append([name,score])
+        
+    grades = [score for name,score in list1]
+    unique_score = []
+    for scores in grades:
+        if scores not in unique_score:
+            unique_score.append(scores)
+    
+    unique_score.sort()
+    second_lowest = unique_score[1]
+    
+    names = []
+    
+    for name, score in list1:
+        if score == second_lowest:
+            names.append(name)
+            
+    names.sort()
+    
+    for name in names:
+        print(name) 
+            
+
+            
+
+            
+
+            
+
+            
