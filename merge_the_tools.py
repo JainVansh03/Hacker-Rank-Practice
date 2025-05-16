02a346386f7f2bd34a6040d2dd7957d2766583ce
Vansh Jain <131325445+JainVansh03@users.noreply.github.com>
2025-05-16 21:33:52 +0530

    Update merge_the_tools.py

--- merge_the_tools.py
+++ merge_the_tools.py
@@ -3,7 +3,7 @@ def merge_the_tools(string, k):
     # your code goes here
     i = 0
     while i < len(string):
-        word1 = "".join(OrderedDict.fromkeys(string[i: i+k]))      
+        word1 = "".join(OrderedDict.fromkeys(string[i: i+k]))       
         print(word1)
         i = i + k
 
